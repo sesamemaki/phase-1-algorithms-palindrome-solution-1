1898e670eca644fc4b848f67309c3d8777f77b23
Burcu Bayraö <burju7@yahoo.com>
2023-06-12 10:02:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversed = word.split("").reverse().join("");
+  return word === reversed;
 }
 
 /* 
   Add your pseudocode here
+  
+  reverse the input string
+    if the reversed string is the same as the input
+      return true
+    else
+      return false
+
 */
 
 /*
   Add written explanation of your solution here
+  First word is needed to be split to its letter
+  Reversed and order in the same order
+  Check word is same previous one
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +32,23 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
 }
 
 module.exports = isPalindrome;
